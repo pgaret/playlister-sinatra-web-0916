51fcfa251f3ea64f214f553081ebf25e2493dc7a
calebcox725 <caleb.cox@flatironschool.com>
2016-10-17 16:00:52 -0400

    Build seeds.rb to fill development.db with dummy data

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,8 +4,20 @@ class SongsController < ApplicationController
     erb :'/songs/index'
   end
 
+  get '/songs/new' do
+    @artists = Artist.all
+    @genres = Genre.all
+    erb :'/songs/new'
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
   end
+
+  post '/songs' do    
+    song = Song.create(params[:song])
+    redirect to "/songs/#{song.slug}"
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,12 @@
-# Add seed data here. Seed your database with `rake db:seed`
+require_relative '../config/environment'
 
-dir = Dir.entries("./data")
-binding.pry
+songlist = Dir.entries('db/data').select {|entry| entry.include?("mp3")}.map do |song|
+  song.gsub!(/[\[\]]/, " - ").split(" - ").take(3)
+end
+
+songlist.each do |songs|
+  artist = Artist.find_or_create_by(name: songs[0])
+  genre = Genre.find_or_create_by(name: songs[2])
+  song = Song.find_or_create_by(name: songs[1].strip, artist_id: artist.id)
+  sg = SongGenre.find_or_create_by(genre_id: genre.id, song_id: song.id)
+end
