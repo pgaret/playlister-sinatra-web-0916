9247addfe0acc9d8bf395efe857ea965637549e2
calebcox725 <caleb.cox@flatironschool.com>
2016-10-17 17:00:48 -0400

    Works, but is cheating...

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,8 +15,38 @@ class SongsController < ApplicationController
     erb :'/songs/show'
   end
 
-  post '/songs' do    
+  post '/songs' do 
     song = Song.create(params[:song])
+
+    params[:genres].each do |genre|
+      SongGenre.create(song_id: song.id, genre_id: genre.to_i)
+    end
+
+    song.artist = Artist.find_or_create_by(params[:artist])
+    song.save
+
+    redirect to "/songs/#{song.slug}"
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    @genres = Genre.all
+    erb :'/songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    song = Song.find_by_slug(params[:slug])
+    
+    song.name = params[:song][:name]
+    song.artist = Artist.find_or_create_by(params[:artist])
+
+    SongGenre.where("song_id = ?", song.id).destroy_all
+    params[:genres].each do |genre|
+      SongGenre.create(song_id: song.id, genre_id: genre.to_i)
+    end
+
+    song.save
+
     redirect to "/songs/#{song.slug}"
   end
 
